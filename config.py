0f29b7cb18aa118eee264c736e59021f2e071553
vishnu24102005 <vishnusanjay605@gmail.com>
2026-06-25 01:09:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 load_dotenv()
 
 ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://your-vercel-app.vercel.app"
 ]
 
 DATABASE_URL = os.getenv("DATABASE_URL")
